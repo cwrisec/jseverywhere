ff6974382892efaa3159ae00d26a6bf704783205
Christophe Maertens <Chris@Macbook.local>
2020-07-09 20:49:23 +0200

    GraphQL : mutation

--- src/index.js
+++ src/index.js
@@ -9,19 +9,32 @@ const app = express();
 // Run the server on port in the .env file or 4000
 const port = process.env.PORT || 4000;
 
-
-
+let notes = [
+    { id : '1', content: 'This is a note', author: 'Auth 1'},
+    { id : '2', content: 'This is a another note', author: 'Auth 2'},
+    { id : '3', content: 'Oh again, a note', author: 'Auth 3'},
+    { id : '4', content: 'Neets', author: 'Auth 3'},
+];
 // Construct a schema using GraphQL schema language
 const typeDefs = gql`
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
     type Query {
         hello: String
+        notes: [Note!]!
+        note(id: ID!): Note!
     }
 `;
 
 // Provide resolver functions for our schema fields
 const resolvers = {
     Query: {
-        hello: () => 'hello World'
+        hello: () => 'hello World',
+        notes: () => notes
+        
     }
 };
 
