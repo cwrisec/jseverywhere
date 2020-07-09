7b4146d403b0a9422e5e854c74db98fcc494e4b8
Christophe Maertens <Chris@Macbook.local>
2020-07-09 19:00:21 +0200

    initial back to dev

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 // index.js
 // This is the main entry point of our application
+
+const express = require('express');
+const app = express();
+
+const { ApolloServer, gql } = require('apollo-server-express');
+const port = process.env.PORT || 4000;
+
+// Construct a schema using GraphQL schema language
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+// Provide resolver functions for our schema fields
+const resolver = {
+    Query: {
+        hello: () => 'hello World'
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolver});
+
+
+app.get('/', (req,res) => res.send('Hello Stinky!!'));
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+
+
+
