c411d1c91a6528bf9c5154966cbf1d50166366fb
Christophe Maertens <Chris@Macbook.local>
2020-07-09 20:38:57 +0200

    GraphQL lab init

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # JavaScript Everywhere API
 
 This repository contains code resolutions of the book i did so far for the API chapters of [_JavaScript Everywhere_](https://www.jseverywhere.io/) by Adam D. Scott, published by O'Reilly Media
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@
 // This is the main entry point of our application
 
 const express = require('express');
+/// GraphQL 
+const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 
-const { ApolloServer, gql } = require('apollo-server-express');
+// Run the server on port in the .env file or 4000
 const port = process.env.PORT || 4000;
+
+
 
 // Construct a schema using GraphQL schema language
 const typeDefs = gql`
@@ -15,18 +19,16 @@ const typeDefs = gql`
 `;
 
 // Provide resolver functions for our schema fields
-const resolver = {
+const resolvers = {
     Query: {
         hello: () => 'hello World'
     }
 };
 
-const server = new ApolloServer({ typeDefs, resolver});
+// Apollo Server Setup
+const server = new ApolloServer({ typeDefs, resolvers});
 
+// Apply the Apollo GraphQL middleware and set the path to /api
 
-app.get('/', (req,res) => res.send('Hello Stinky!!'));
-
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
-
-
-
+server.applyMiddleware({ app, path: '/api'});
+app.listen({ port }, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`));
